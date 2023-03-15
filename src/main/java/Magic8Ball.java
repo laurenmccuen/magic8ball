5f559446d1c117020540edd5d3c2dc9b40b82c52
Lauren McCuen <laurenemccuen@gmail.com>
2023-03-15 08:32:46 -0400

    Magic8 Ball In Progress

--- src/main/java/Magic8Ball.java
+++ src/main/java/Magic8Ball.java
@@ -7,37 +7,53 @@ import java.util.Scanner;
 public class Magic8Ball {
 
     // 1. Make a main method that includes all the steps below
+    public static void main(String[] args) {
 
 
-    // 2. Get the user to enter a question for the magic 8-ball to answer
-    //    Scanner scanner = new Scanner(System.in);
-    //    String userQuestion = scanner.nextLine();
+        // 2. Get the user to enter a question for the magic 8-ball to answer
+        //    Scanner scanner = new Scanner(System.in);
+        //    String userQuestion = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number from 0 to 3: ");
+        String userQuestion = scanner.nextLine();
 
 
-    // 3. Make a variable and initialize it to a random number.
-    //
-    //    // Get random a number from: [0, 1, 2, 3]
-    //    int randNumber = new Random().nextInt(4);
+        // 3. Make a variable and initialize it to a random number.
+        //
+        //    // Get random a number from: [0, 1, 2, 3]
+        //    int randNumber = new Random().nextInt(4);
 
+        int randNumber = new Random().nextInt(4);
 
-    // 4. If the random number is 0
+        // 4. If the random number is 0
 
         // Tell the user "Yes"
+        if (randNumber == 0) {
+            System.out.println("Yes");;
+        } else if (randNumber == 1) {
+            System.out.println("No");
+        } else if (randNumber == 2) {
+            System.out.println("Custom Response");;
+        } else if (randNumber == 3) {
+            System.out.println("Custom Response2");;
+        }
 
-
-    // 5. If the random number is 1
+        // 5. If the random number is 1
 
         // Tell the user "No"
 
 
-    // 6. If the random number is 2
+        // 6. If the random number is 2
 
         // Tell the user a custom response
 
 
-    // 7. If the random number is 3
+        // 7. If the random number is 3
 
         // Tell the user a custom response
+
+    }
 }
 
 

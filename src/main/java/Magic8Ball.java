6e7c7909a21c832fcf31bbe6a6db4f28b4288a94
Lauren McCuen <laurenemccuen@gmail.com>
2023-03-15 10:19:37 -0400

    Magic8 Ball In Progress

--- src/main/java/Magic8Ball.java
+++ src/main/java/Magic8Ball.java
@@ -15,43 +15,55 @@ public class Magic8Ball {
         //    String userQuestion = scanner.nextLine();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number from 0 to 3: ");
+        System.out.println("The Magic 8 Ball knows all! Think of a question you would like answered.");
+        System.out.print("Now, enter a number from 0 to 15 to learn your future: ");
         String userQuestion = scanner.nextLine();
 
 
         // 3. Make a variable and initialize it to a random number.
-        //
         //    // Get random a number from: [0, 1, 2, 3]
         //    int randNumber = new Random().nextInt(4);
+        int randNumber = new Random().nextInt(16);
+        String response = "";
 
-        int randNumber = new Random().nextInt(4);
-
-        // 4. If the random number is 0
-
-        // Tell the user "Yes"
+        // create if statements for the responses
         if (randNumber == 0) {
-            System.out.println("Yes");;
+            response = "It is certain";
         } else if (randNumber == 1) {
-            System.out.println("No");
+            response = "It is decidedly so";
         } else if (randNumber == 2) {
-            System.out.println("Custom Response");;
-        } else if (randNumber == 3) {
-            System.out.println("Custom Response2");;
+            response = "Reply is hazy, try again";
+        }  else if (randNumber == 3) {
+            response = "Yes, definitely";
+        } else if (randNumber == 4) {
+            response = "Don't count on it";
+        } else if (randNumber == 5) {
+            response = "My sources say no";
+        }else if (randNumber == 6) {
+            response = "Cannot predict now";
+        } else if (randNumber == 7) {
+            response = "Signs point to yes";
+        } else if (randNumber == 8) {
+            response = "You may rely on it";
+        } else if (randNumber == 9) {
+            response = "Better not tell you now";
+        } else if (randNumber == 10) {
+            response = "Ask again later";
+        } else if (randNumber == 11) {
+            response = "Very doubtful";
+        } else if (randNumber == 12) {
+            response = "Outlook is good";
+        } else if (randNumber == 13) {
+            response = "Concentrate and ask again";
+        } else if (randNumber == 14) {
+            response = "Most likely";
+        } else if (randNumber == 15) {
+            response = "Without a doubt";
+        } else if (randNumber >= 16) {
+            response = "8-BALL ERROR, Try again!";
         }
+        System.out.println("Magic 8-Ball says: " + response);
 
-        // 5. If the random number is 1
-
-        // Tell the user "No"
-
-
-        // 6. If the random number is 2
-
-        // Tell the user a custom response
-
-
-        // 7. If the random number is 3
-
-        // Tell the user a custom response
 
     }
 }
